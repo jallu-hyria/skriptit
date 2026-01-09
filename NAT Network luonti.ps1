6546a598cb10fd6027bf58d2b1432c4491f2135f
jallu-hyria <jalmari.valimaa@hyria.fi>
2026-01-09 14:04:21 +0200

    Add files via upload

---
+++ NAT Network luonti.ps1
@@ -0,0 +1,11 @@
+﻿# Nat Network luominen Hyper-V:hen
+
+# Windows kenraali 10.100.0.0 /22
+New-VMSwitch -SwitchName "Windows kenraali verkko" -SwitchType Internal
+New-NetIPAddress -IPAddress 10.100.0.1 -PrefixLength 22 -InterfaceAlias "vEthernet (Windows kenraali verkko)"
+New-NetNat -Name "Windows kenraali NAT" -InternalIPInterfaceAddressPrefix 10.100.0.0/22
+
+# Linux kenraali 10.220.132.0 /22
+New-VMSwitch -SwitchName "Linux kenraali verkko" -SwitchType Internal
+New-NetIPAddress -IPAddress 10.220.133.1 -PrefixLength 22 -InterfaceAlias "vEthernet (Linux kenraali verkko)"
+New-NetNat -Name "Linux kenraali NAT" -InternalIPInterfaceAddressPrefix 10.220.132.0/22
