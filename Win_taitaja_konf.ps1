a6459f66eaf3279c098676a4009c72118029b4e7
jallu-hyria <jalmari.valimaa@hyria.fi>
2026-01-26 14:40:10 +0200

    Update Win_taitaja_konf.ps1
    AutoInstall>true< turha välilyönti edessä poistettu

--- Win_taitaja_konf.ps1
+++ Win_taitaja_konf.ps1
@@ -877,7 +877,7 @@ do
     #GPO asenna Chrome
     $ChromeAsennus = "ChromeAsennus"
     Get-GPOReport -Name $ChromeAsennus -ReportType XML -Path C:\ChromeAsennus.xml
-    $ChromeAsennusGPO = Select-String -path C:\ChromeAsennus.xml -Pattern 'Name>Google Chrome<' -SimpleMatch -Context 0, 12 | Out-String -Stream | Select-String -Pattern " AutoInstall>true<"
+    $ChromeAsennusGPO = Select-String -path C:\ChromeAsennus.xml -Pattern 'Name>Google Chrome<' -SimpleMatch -Context 0, 12 | Out-String -Stream | Select-String -Pattern "AutoInstall>true<"
     if ($ChromeAsennusGPO -eq $null)
         {
         Write-Host "GPOn $ChromeAsennus sääntö näyttäisi olevan väärin tehty." -ForegroundColor Red
@@ -1766,6 +1766,7 @@ do
 
  }
  until ($selection -eq 'q')
+
 
 
 
